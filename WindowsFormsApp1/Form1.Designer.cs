3cddc910fda187827efbfb727c317eeaeda686fb
Liam <1mailliam1@gmail.com>
2021-01-19 09:28:56 -0800

    initial commit

---
+++ WindowsFormsApp1/Form1.Designer.cs
@@ -0,0 +1,235 @@
+﻿namespace WindowsFormsApp1
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.button1 = new System.Windows.Forms.Button();
+            this.username = new System.Windows.Forms.TextBox();
+            this.password = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.label6 = new System.Windows.Forms.Label();
+            this.label7 = new System.Windows.Forms.Label();
+            this.label8 = new System.Windows.Forms.Label();
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.label9 = new System.Windows.Forms.Label();
+            this.label10 = new System.Windows.Forms.Label();
+            this.label11 = new System.Windows.Forms.Label();
+            this.panel1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(202, 41);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(112, 74);
+            this.button1.TabIndex = 0;
+            this.button1.Text = "Get Photos!";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // username
+            // 
+            this.username.Location = new System.Drawing.Point(19, 41);
+            this.username.Name = "username";
+            this.username.Size = new System.Drawing.Size(177, 20);
+            this.username.TabIndex = 1;
+            this.username.Text = "username";
+            // 
+            // password
+            // 
+            this.password.Location = new System.Drawing.Point(19, 95);
+            this.password.Name = "password";
+            this.password.Size = new System.Drawing.Size(177, 20);
+            this.password.TabIndex = 2;
+            this.password.Text = "password";
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(40, 9);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(64, 13);
+            this.label1.TabIndex = 3;
+            this.label1.Text = "Instructions:";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(40, 55);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(477, 13);
+            this.label2.TabIndex = 4;
+            this.label2.Text = "Enter your facebook username and password below. (Don\'t worry, they will not be s" +
+    "tored anywhere.)";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(40, 75);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(89, 13);
+            this.label3.TabIndex = 5;
+            this.label3.Text = "Click Get Photos!";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(40, 95);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(319, 13);
+            this.label4.TabIndex = 6;
+            this.label4.Text = "Drink the tea to avoid becoming mesmerized by photos of yourself.";
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(41, 155);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(354, 13);
+            this.label5.TabIndex = 7;
+            this.label5.Text = "Take a look in the \"Photos of you!\" directory created next to this program.";
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Location = new System.Drawing.Point(41, 175);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(383, 13);
+            this.label6.TabIndex = 8;
+            this.label6.Text = "If photos are there and you\'ve already downloaded other data, delete facebook!";
+            // 
+            // label7
+            // 
+            this.label7.AutoSize = true;
+            this.label7.Location = new System.Drawing.Point(41, 195);
+            this.label7.Name = "label7";
+            this.label7.Size = new System.Drawing.Size(242, 13);
+            this.label7.TabIndex = 9;
+            this.label7.Text = "Enjoy the freedom of life in a post facebook world.";
+            // 
+            // label8
+            // 
+            this.label8.AutoSize = true;
+            this.label8.Location = new System.Drawing.Point(41, 35);
+            this.label8.Name = "label8";
+            this.label8.Size = new System.Drawing.Size(91, 13);
+            this.label8.TabIndex = 10;
+            this.label8.Text = "Brew a cup of tea";
+            // 
+            // panel1
+            // 
+            this.panel1.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.panel1.Controls.Add(this.button1);
+            this.panel1.Controls.Add(this.username);
+            this.panel1.Controls.Add(this.password);
+            this.panel1.Location = new System.Drawing.Point(44, 265);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(324, 163);
+            this.panel1.TabIndex = 11;
+            // 
+            // label9
+            // 
+            this.label9.AutoSize = true;
+            this.label9.Location = new System.Drawing.Point(41, 115);
+            this.label9.Name = "label9";
+            this.label9.Size = new System.Drawing.Size(726, 13);
+            this.label9.TabIndex = 12;
+            this.label9.Text = "Note that this will NOT download videos (facebook purposefully makes doing so dif" +
+    "ficult) but there are browser extensions that do it, google for directions.";
+            // 
+            // label10
+            // 
+            this.label10.AutoSize = true;
+            this.label10.Location = new System.Drawing.Point(40, 135);
+            this.label10.Name = "label10";
+            this.label10.Size = new System.Drawing.Size(683, 13);
+            this.label10.TabIndex = 13;
+            this.label10.Text = "This program will also generate a list of links to all videos of you in a file ca" +
+    "lled \"video-urls.txt\" in case that is helpful to find them for downloading.";
+            // 
+            // label11
+            // 
+            this.label11.AutoSize = true;
+            this.label11.Location = new System.Drawing.Point(41, 227);
+            this.label11.Name = "label11";
+            this.label11.Size = new System.Drawing.Size(254, 13);
+            this.label11.TabIndex = 14;
+            this.label11.Text = "Done downloading! You can close this window now.";
+            this.label11.Visible = false;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(828, 440);
+            this.Controls.Add(this.label11);
+            this.Controls.Add(this.label10);
+            this.Controls.Add(this.label9);
+            this.Controls.Add(this.panel1);
+            this.Controls.Add(this.label8);
+            this.Controls.Add(this.label7);
+            this.Controls.Add(this.label6);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Name = "Form1";
+            this.Text = "Facebook photo download tool";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.panel1.ResumeLayout(false);
+            this.panel1.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.TextBox username;
+        private System.Windows.Forms.TextBox password;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.Label label7;
+        private System.Windows.Forms.Label label8;
+        private System.Windows.Forms.Panel panel1;
+        private System.Windows.Forms.Label label9;
+        private System.Windows.Forms.Label label10;
+        private System.Windows.Forms.Label label11;
+    }
+}
+
---
+++ WindowsFormsApp1/Form1.cs
@@ -0,0 +1,141 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Net;
+
+using OpenQA.Selenium;
+using OpenQA.Selenium.Chrome;
+using OpenQA.Selenium.Support.UI;
+using System.IO;
+
+namespace WindowsFormsApp1
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            IWebDriver b;
+
+            try
+            {
+                var chromeDriverService = ChromeDriverService.CreateDefaultService("chromedriver87");
+                chromeDriverService.HideCommandPromptWindow = true;
+                ChromeOptions options = new ChromeOptions();
+                options.AddArguments("--disable-notifications"); // to disable notification
+
+                b = new ChromeDriver(chromeDriverService, options);
+            } catch
+            {
+                var chromeDriverService = ChromeDriverService.CreateDefaultService("chromedriver88");
+                chromeDriverService.HideCommandPromptWindow = true;
+                ChromeOptions options = new ChromeOptions();
+                options.AddArguments("--disable-notifications"); // to disable notification
+
+                b = new ChromeDriver(chromeDriverService, options);
+            }
+
+            b.Manage().Window.Maximize();
+
+            b.Navigate().GoToUrl("https://facebook.com");
+
+            b.FindElement(By.Id("email")).SendKeys(username.Text);
+            b.FindElement(By.Id("pass")).SendKeys(password.Text);
+            b.FindElement(By.Name("login")).Click();
+
+            ExplicitWait(b, "//*[@id=\"mount_0_0\"]/div/div[1]/div[1]/div[3]/div/div/div[1]/div[1]/div/div[1]/div/div/div[1]/div/div/div[1]/ul/li/div/a/div[1]/div[2]/div/div/div/div/span/span").Click();
+
+            String base_url = b.Url;
+
+            b.Url = base_url + "/photos";
+            ExplicitWait(b, "//*[@id=\"mount_0_0\"]/div/div[1]/div[1]/div[3]/div/div/div[1]/div[1]/div/div/div[4]/div/div/div/div/div/div/div/div[3]/div[1]/div[1]/div/div/a/img").Click();
+
+            WebClient download_client = new WebClient();
+
+            string previous_src = "";
+            string src = "";
+
+            System.IO.Directory.CreateDirectory("Photos of you!");
+
+            for (int i = 0; i < 10000; i++)
+            {
+                // detect video
+                if (b.Url.Contains("/videos/"))
+                {
+                    string path = "video-urls.txt";
+                    if (!File.Exists(path))
+                    {
+                        // Create a file to write to.
+                        using (StreamWriter sw = File.CreateText(path))
+                        {
+                            sw.WriteLine(b.Url);
+                        }
+                    }
+                    else
+                    {
+                        using (StreamWriter sw = File.AppendText(path))
+                        {
+                            sw.WriteLine(b.Url);
+                        }
+                    }
+                    i--;
+                    b.FindElement(By.TagName("body")).SendKeys(OpenQA.Selenium.Keys.Right);
+                    continue;
+                }
+
+
+                for (int j = 0; j < 20; j++)
+                {
+                    try
+                    {
+                        src = ExplicitWait(b, "//*[@id=\"mount_0_0\"]/div/div[1]/div[1]/div[4]/div/div/div[1]/div/div[3]/div[2]/div/div[2]/div/div[1]/div/div[2]/div/img").GetAttribute("src");
+                    }
+                    catch (OpenQA.Selenium.StaleElementReferenceException stalerefexception)
+                    {
+                        System.Threading.Thread.Sleep(200);
+                    }
+                }
+
+                if (src == previous_src)
+                {
+                    break;
+                }
+
+                string title = "Photos of you!/photo" + i + ".png";
+
+                download_client.DownloadFile(src, title);
+
+                previous_src = src;
+
+                b.FindElement(By.TagName("body")).SendKeys(OpenQA.Selenium.Keys.Right);
+
+            }
+
+            b.Quit();
+            label11.Visible = true;
+        }
+
+
+        public static IWebElement ExplicitWait(IWebDriver b, string xpath)
+        {
+            return new WebDriverWait(b, TimeSpan.FromSeconds(5)).Until(ExpectedConditions.ElementToBeClickable(By.XPath(xpath)));
+        }
+
+    }
+
+
+}
